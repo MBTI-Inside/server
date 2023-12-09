e72a99221a13fdcc2d2a77f139ad293d54140724
rebi13 <rebi13@naver.com>
2023-12-10 02:33:25 +0900

    first init

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <section className="flex flex-col items-center">
+      <h3 className="text-2xl font-bold mb-2">NotFound😅</h3>
+    </section>
+  );
+}
