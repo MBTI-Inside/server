83e8b0ad985dee4f6af6331a81bfdff3ccd24264
rebi13 <rebi13@naver.com>
2023-12-10 16:08:58 +0900

    feat: Header, Footer 구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import '@/App.css';
-import { RouterProvider } from 'react-router-dom';
 import router from '@/routers';
+import { RouterProvider } from 'react-router-dom';
+
+import Footer from '@/components/layout/Footer';
+import Header from '@/components/layout/Header';
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <>
+      <Header></Header>
+      <RouterProvider router={router}></RouterProvider>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
