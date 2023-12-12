089d5dadba7f4b112eb29f72a94a2ed3c62aef0d
rebi13 <rebi13@naver.com>
2023-12-12 23:34:01 +0900

    style: tailwindcss import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRoot } from 'react-dom/client';
 
+import './index.css';
+
 const root = document.getElementById('root') as HTMLElement;
 
 const rootElement = createRoot(root);
