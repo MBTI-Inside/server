c062b9c27dce3614aedb51f38c99b8d8d83031f1
rebi13 <rebi13@naver.com>
2023-12-12 23:32:08 +0900

    setting: vite-plugin-svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import requireTransform from 'vite-plugin-require-transform';
+// import requireTransform from 'vite-plugin-require-transform';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), requireTransform({})],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   }
