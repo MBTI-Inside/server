e022cb660eaa3504028b57eea7ef4bcf7bdfb8d2
rebi13 <rebi13@naver.com>
2023-12-12 23:33:24 +0900

    style: 사용하지 않는 import 제거

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,5 +1,4 @@
 import { lazy } from 'react';
-import { createBrowserRouter } from 'react-router-dom';
 
 const Home = lazy(() => import('@/pages/Home'));
 const NotFound = lazy(() => import('@/pages/NotFound'));
