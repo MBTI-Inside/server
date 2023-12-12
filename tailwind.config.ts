84e55c6703007b2801fb2a8a494971671bb1b25c
rebi13 <rebi13@naver.com>
2023-12-12 23:33:34 +0900

    setting: tailwind 세팅 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,23 @@
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        brand: '#329dfa',
+        'black-semi-transparent': 'rgba(0,0,0,0.7)',
+        'regal-yellow': '#FFDF3F',
+        'regal-purple': '#B2ACF9'
+      },
+      backgroundImage: {
+        // main: `url('${import.meta.env.VITE_PUBLIC_URL}/main.svg')`
+        // test: `url('${process.env.REACT_APP_PUBLIC_URL}/test.svg')`
+      },
+      fontSize: {
+        vxs: '0.6rem'
+      },
+      keyframes: {},
+      animation: {}
+    }
   },
   plugins: [require('daisyui')]
 };
