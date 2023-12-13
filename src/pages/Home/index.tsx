b0505c5b1539015846f0a4810e1d803b16b2f419
rebi13 <rebi13@naver.com>
2023-12-14 00:37:43 +0900

    feat: Home 임시 커밋

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import * as S from './index.styles';
+
 const Home = () => {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <S.Background>
+      <S.Container>
+        <S.Character />
+        <S.ContentWrapper>
+          <S.Title>ARE YOU T?</S.Title>
+          <Link to="/test">
+            <S.FirstButton>테스트 하러가기</S.FirstButton>
+          </Link>
+          <Link to="/stats">
+            <S.Button>통계 보러가기</S.Button>
+          </Link>
+          <Link to="/board">
+            <S.Button>담벼락 보러가기</S.Button>
+          </Link>
+        </S.ContentWrapper>
+      </S.Container>
+    </S.Background>
   );
 };
 
