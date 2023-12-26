c5cbf7d4d63e2462ffc8bf360635d58b51dece79
rebi13 <rebi13@naver.com>
2023-12-26 22:56:39 +0900

    setting: meta 설정 제거

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-interface ImportMetaEnv {
-  readonly VITE_CUSTOM_ENV_VARIABLE: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
