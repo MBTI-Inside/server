1c0ed5148b4ae040c50b5897e4a35b014c29b191
rebi13 <rebi13@naver.com>
2023-12-30 16:39:59 +0900

    style: 공통 header 스타일링 rebi13/MBTI-Inside#2

--- frontend/src/components/layout/Header/index.styles.ts
+++ frontend/src/components/layout/Header/index.styles.ts
@@ -1,98 +1,34 @@
 import { BsList } from 'react-icons/bs';
+import { FaRegCircleUser } from 'react-icons/fa6';
+import { FiLogIn } from 'react-icons/fi';
 import tw from 'tailwind-styled-components';
 
-export const Logo = tw.span`
+export const HeaderContainer = tw.header`
   flex
-  w-36
-  h-auto
-  items-center
-  gap-1.5
-  shrink-0
-  flex-start
-`;
-
-export const ScaledBsList = tw(BsList)`
-text-4xl
-text-black
-`;
-
-export const HeaderWrapper = tw.header`
-  flex
-  flex-wrap
-  w-full
-  m-auto
-  h-auto
-  shrink-0
   bg-regal-purple
   p-2.5
   items-center
   justify-between
 `;
 
-export const Drawer = tw.span`
-  flex
-  w-auto
-  h-auto
-  h-9
+export const InterfaceContainer = tw.div`
+flex
+w-20
+justify-between
 `;
 
-export const DrawerButton = tw.label`
-  btn
-  drawer-button
-  border-none
-bg-transparent
-p-0
-min-h-0
-h-full
+// TODO: 겹치는 style을 공통으로 사용할 수 없을까?
+export const LoginIcon = tw(FiLogIn)`
+text-4xl
+text-black
 `;
 
-export const DrawerToggle = tw.input`
-  drawer-toggle
-  text-black
+export const UserIcon = tw(FaRegCircleUser)`
+text-4xl
+text-black
 `;
 
-export const DrawerOverlay = tw.label`
-  drawer-overlay
-`;
-
-export const DrawerContent = tw.div`
-  drawer-content
-  h-9
-`;
-
-export const DrawerSide = tw.div`
-  drawer-side
-  z-50
-`;
-
-export const SideBarContent = tw.ul`
-  menu
-  p-4
-  w-80
-  h-full
-  bg-base-200
-  text-base-content
-  bg-regal-purple
-`;
-
-export const SiderBarItem = tw.li`
-  mt-9
-  btn
-  btn-wide
-  w-full
-  h-14
-  flex-shrink-0
-  rounded-full
-  shadow-lg
-  border-none
-`;
-
-export const SideBarFooter = tw.li`
-  flex
-  mt-auto
-  pointer-events-none
-`;
-
-export const SideBarFooterContent = tw.div`
-  self-center
+export const MenuIcon = tw(BsList)`
+text-4xl
+text-black
 `;
--- frontend/src/components/layout/Header/index.tsx
+++ frontend/src/components/layout/Header/index.tsx
@@ -1,90 +1,30 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import MainLogoSvg from '@/assets/image/mainlogo.svg?react';
 
 import {
-  Drawer,
-  DrawerButton,
-  DrawerContent,
-  DrawerOverlay,
-  DrawerSide,
-  DrawerToggle,
-  HeaderWrapper,
-  Logo,
-  ScaledBsList,
-  SideBarContent,
-  SideBarFooter,
-  SideBarFooterContent,
-  SiderBarItem
+  HeaderContainer,
+  InterfaceContainer,
+  LoginIcon,
+  MenuIcon,
+  UserIcon
 } from './index.styles';
 
 const Header = () => {
-  const [isChecked, setIsChecked] = useState(false);
-  // 이벤트 핸들러 함수 추가
-  const handleDrawerToggle = () => {
-    setIsChecked(!isChecked);
-  };
   return (
-    <HeaderWrapper>
-      <Link to="/">
-        <Logo className="flex-start"></Logo>
+    <HeaderContainer>
+      <NavLink to="/">
         <MainLogoSvg />
-      </Link>
-      <Drawer>
-        <DrawerToggle
-          id="my-drawer"
-          type="checkbox"
-          checked={isChecked}
-          readOnly
-        ></DrawerToggle>
-        <DrawerContent onClick={handleDrawerToggle}>
-          <DrawerButton htmlFor="my-drawer">
-            <ScaledBsList></ScaledBsList>
-          </DrawerButton>
-        </DrawerContent>
-        <DrawerSide>
-          <DrawerOverlay
-            htmlFor="my-drawer"
-            onClick={handleDrawerToggle}
-          ></DrawerOverlay>
-          <SideBarContent>
-            <Link to="/" onClick={handleDrawerToggle}>
-              <Logo></Logo>
-            </Link>
-            <Link to="/test" onClick={handleDrawerToggle}>
-              <SiderBarItem className="bg-regal-yellow text-black">
-                테스트 하러가기
-              </SiderBarItem>
-            </Link>
-            <Link to="/stats" onClick={handleDrawerToggle}>
-              <SiderBarItem className="bg-black text-white">
-                통계 보러가기
-              </SiderBarItem>
-            </Link>
-            <Link to="/board" onClick={handleDrawerToggle}>
-              <SiderBarItem className="bg-black text-white">
-                담벼락 보러가기
-              </SiderBarItem>
-            </Link>
-            <SideBarFooter>
-              <SideBarFooterContent className="text-black">
-                AYT Company
-              </SideBarFooterContent>
-              <SideBarFooterContent className="pointer-events-auto text-black">
-                <a
-                  href="https://github.com/rebi13/MBTI-Inside"
-                  target="_blank"
-                  rel="MBTI-Inside noreferrer"
-                >
-                  https://github.com/rebi13/MBTI-Inside
-                </a>
-              </SideBarFooterContent>
-            </SideBarFooter>
-          </SideBarContent>
-        </DrawerSide>
-      </Drawer>
-    </HeaderWrapper>
+      </NavLink>
+
+      <InterfaceContainer>
+        {/* 로그인 시 표시*/}
+        {/* <UserIcon /> */}
+        <LoginIcon />
+        <MenuIcon />
+      </InterfaceContainer>
+    </HeaderContainer>
   );
 };
 
