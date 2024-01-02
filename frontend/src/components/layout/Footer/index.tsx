4041badb15c1fe58ae7ee9c564fd209538dbfb3c
rebi13 <rebi13@naver.com>
2024-01-02 21:26:38 +0900

    feat: Web Share API 적용 rebi13/MBTI-Inside#14

--- frontend/src/components/layout/Footer/index.tsx
+++ frontend/src/components/layout/Footer/index.tsx
@@ -11,37 +11,37 @@ import {
 } from './index.styles';
 
 export default function Footer() {
-  // Web Share API 적용코드
-  // const handleShareClick = async () => {
-  //   if (navigator.share) {
-  //     try {
-  //       await navigator.share({
-  //         title: document.title,
-  //         text: '링크를 공유합니다.',
-  //         url: window.location.href
-  //       });
-  //       console.log('링크를 공유했습니다!');
-  //     } catch (error) {
-  //       console.error('링크 공유에 실패했습니다.', error);
-  //     }
-  //   } else {
-  //     // Web Share API를 지원하지 않는 경우
-  //     // 대체 동작을 수행합니다.
-  //     await share(window.location.origin);
-  //   }
-  // };
+  // Web Share API 적용
+  const handleShareClick = async () => {
+    // TODO: 배포 후 모바일 환경에서 확인할 것.
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: document.title,
+          text: '링크를 공유합니다.',
+          url: window.location.href
+        });
+        console.log('링크를 공유했습니다!');
+      } catch (error) {
+        // TODO: error 처리
+        console.error('링크 공유에 실패했습니다.', error);
+      }
+    } else {
+      // Web Share API를 지원하지 않는 경우
+      // 대체 동작을 수행합니다.
+      await share(window.location.origin);
+    }
+  };
 
-  // const share = async (text: string) => {
-  //   try {
-  //     await navigator.clipboard.writeText(text);
-  //     alert('링크가 복사되었습니다!');
-  //   } catch (e) {
-  //     alert('초대코드 복사에 실패했습니다ㅜㅜ');
-  //   }
-  // };
-  // const handleShareButtonClick = () => {
-  //   handleShareClick();
-  // };
+  const share = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert('링크가 복사되었습니다!');
+    } catch (e) {
+      // TODO: error 처리
+      alert('초대코드 복사에 실패했습니다ㅜㅜ');
+    }
+  };
 
   return (
     // <FooterWrapper>
@@ -54,9 +54,9 @@ export default function Footer() {
 
     //   <FooterLinkIcon>
     //     <FooterLink
-    //       href="https://github.com/rebi13/MBTI-Inside"
-    //       target="_blank"
-    //       rel="MBTI-Inside noreferrer"
+    // href="https://github.com/rebi13/MBTI-Inside"
+    // target="_blank"
+    // rel="MBTI-Inside noreferrer"
     //     >
     //       <GitHubSvg />
     //     </FooterLink>
@@ -75,12 +75,16 @@ export default function Footer() {
         </FooterText>
       </FooterTextArea>
       <FooterNav>
-        <a>
+        <FooterLink
+          href="https://github.com/rebi13/MBTI-Inside"
+          target="_blank"
+          rel="MBTI-Inside noreferrer"
+        >
           <GitHubIcon />
-        </a>
-        <a>
+        </FooterLink>
+        <FooterLink onClick={handleShareClick}>
           <ShareIcon />
-        </a>
+        </FooterLink>
       </FooterNav>
     </FooterContainer>
   );
