c65eb48cf890c87a905c77b40f438c2027c447ad
rebi13 <rebi13@naver.com>
2024-01-24 00:00:14 +0900

    feat: memo 라우터 추가 rebi13/MBTI-Inside#29

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -1,12 +1,18 @@
 import { lazy } from 'react';
 
 const Home = lazy(() => import('@/pages/Home'));
+const Memo = lazy(() => import('@/pages/Memo'));
+
 const NotFound = lazy(() => import('@/pages/NotFound'));
 
 const routePaths = [
   {
     path: '/',
     element: <Home />
+  },
+  {
+    path: '/memo',
+    element: <Memo />
   },
   {
     path: '/notfound',
