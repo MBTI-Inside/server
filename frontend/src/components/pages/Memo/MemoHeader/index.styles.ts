ce7da1b37d65d3ea070b143ce0589c99cc23ed43
rebi13 <rebi13@naver.com>
2024-01-30 21:49:26 +0900

    style: 메모 헤더 영역 스타일 rebi13/MBTI-Inside#31

---
+++ frontend/src/components/pages/Memo/MemoHeader/index.styles.ts
@@ -0,0 +1,17 @@
+import tw from 'tailwind-styled-components';
+
+export const MbtiTitle = tw.div`
+ flex 
+ flex-col 
+ items-center 
+ p-3
+ justify-between
+`;
+
+export const Title = tw.div`
+  text-4xl
+  font-bold
+`;
+
+export const SearchBadges = tw.div`
+`;
