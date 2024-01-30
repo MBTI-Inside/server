784ca46eff3e0a2917ae635f99366f16b944229a
rebi13 <rebi13@naver.com>
2024-01-30 23:04:38 +0900

    docs: 모달 스토리북 rebi13/MBTI-Inside#26

--- frontend/src/components/common/Modal/index.stories.tsx
+++ frontend/src/components/common/Modal/index.stories.tsx
@@ -1,14 +1,20 @@
-import { StoryFn } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import Modal from './index';
 
-export default {
+const meta = {
   title: 'components/common/Modal',
-  component: Modal
+  component: Modal,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs'],
+  argTypes: {}
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultModal: Story = {
+  args: {}
 };
-
-const Template: StoryFn<any> = (args) => <Modal {...args} />;
-
-export const Default = Template.bind({});
-
-Default.args = {};
