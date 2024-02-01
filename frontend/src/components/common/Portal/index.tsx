184c6a8ed0ed9696f79ceacd32483423bf5ba5c7
rebi13 <rebi13@naver.com>
2024-02-01 22:53:18 +0900

    feat: FC 네이밍 변경 rebi13/MBTI-Inside#26

--- frontend/src/components/common/Portal/index.tsx
+++ frontend/src/components/common/Portal/index.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
-interface ModalPortalProps {
+interface PortalProps {
   children: ReactNode;
 }
 
-const ModalPortal: React.FC<ModalPortalProps> = ({ children }) => {
+const Portal: React.FC<PortalProps> = ({ children }) => {
   const el = document.getElementById('modal');
   if (!el) return null; // 대상 요소가 존재하지 않음
   return createPortal(children, el);
 };
 
-export default ModalPortal;
+export default Portal;
