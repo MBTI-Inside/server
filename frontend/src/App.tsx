9d1b203c97665a4bc341367483402d98a808ebff
rebi13 <rebi13@naver.com>
2024-02-02 00:09:43 +0900

    feat: 모달 provider 전역에 위치 rebi13/MBTI-Inside#26

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import routePaths from '@/routers';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ModalProvider from '@/hooks/useModal';
+
 import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 
@@ -8,13 +10,19 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          {routePaths.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-        </Routes>
-        <Footer />
+        <ModalProvider>
+          <Header />
+          <Routes>
+            {routePaths.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+          <Footer />
+        </ModalProvider>
       </BrowserRouter>
     </>
   );
