c99c700c0aa8e299dfc0bf0ea72d7a55b95eef47
rebi13 <rebi13@naver.com>
2024-02-11 22:16:01 +0900

    style: 색상 추가 rebi13/MBTI-Inside#37

--- frontend/src/components/common/ColorChip/index.tsx
+++ frontend/src/components/common/ColorChip/index.tsx
@@ -22,6 +22,38 @@ const ColorChip = () => {
           <FaCheck />
         </label>
       </div>
+      <div className="flex items-center mt-5">
+        <input type="radio" name="colors" className="hidden" />
+        <label className="flex items-center cursor-pointer flex-1 gap-4">
+          <S.chip bg={'bg-[#B2ACF9]'} />
+          <span>퍼플</span>
+          <FaCheck />
+        </label>
+      </div>
+      <div className="flex items-center mt-5">
+        <input type="radio" name="colors" className="hidden" />
+        <label className="flex items-center cursor-pointer flex-1 gap-4">
+          <S.chip bg={'bg-[#9FEEA2]'} />
+          <span>그린</span>
+          <FaCheck />
+        </label>
+      </div>
+      <div className="flex items-center mt-5">
+        <input type="radio" name="colors" className="hidden" />
+        <label className="flex items-center cursor-pointer flex-1 gap-4">
+          <S.chip bg={'bg-[#78D9EE]'} />
+          <span>블루</span>
+          <FaCheck />
+        </label>
+      </div>
+      <div className="flex items-center mt-5">
+        <input type="radio" name="colors" className="hidden" />
+        <label className="flex items-center cursor-pointer flex-1 gap-4">
+          <S.chip bg={'bg-[#FF9D42]'} />
+          <span>오렌지</span>
+          <FaCheck />
+        </label>
+      </div>
     </div>
   );
 };
