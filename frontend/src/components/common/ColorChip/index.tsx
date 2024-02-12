5d3c615aacc7da20d6012a4263942c7ccda40e71
rebi13 <rebi13@naver.com>
2024-02-12 21:36:06 +0900

    feat: 코드 정리. 선택 기능 추가 rebi13/MBTI-Inside#37

--- frontend/src/components/common/ColorChip/index.tsx
+++ frontend/src/components/common/ColorChip/index.tsx
@@ -1,59 +1,54 @@
+import { useState } from 'react';
 import { FaCheck } from 'react-icons/fa';
 
+import Button from '@/components/common/Button';
 import * as S from '@/components/common/ColorChip/index.styles';
 
+import { bgColors } from '@/constants/bgColors';
+
 const ColorChip = () => {
+  const [color, setColor] = useState('yellow');
+
+  const doSomething = () => {
+    console.log(color);
+  };
   return (
     <div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#FFDE3F]'} />
-          <span>옐로우</span>
-          <FaCheck />
-        </label>
-      </div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#EFC7D6]'} />
-          <span>핑크</span>
-          <FaCheck />
-        </label>
-      </div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#B2ACF9]'} />
-          <span>퍼플</span>
-          <FaCheck />
-        </label>
-      </div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#9FEEA2]'} />
-          <span>그린</span>
-          <FaCheck />
-        </label>
-      </div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#78D9EE]'} />
-          <span>블루</span>
-          <FaCheck />
-        </label>
-      </div>
-      <div className="flex items-center mt-5">
-        <input type="radio" name="colors" className="hidden" />
-        <label className="flex items-center cursor-pointer flex-1 gap-4">
-          <S.chip bg={'bg-[#FF9D42]'} />
-          <span>오렌지</span>
-          <FaCheck />
-        </label>
-      </div>
+      {bgColors.map(({ colorId, bgColor, name }) => (
+        <div className="flex items-center mt-5" key={colorId}>
+          <input
+            type="radio"
+            id={colorId}
+            value={colorId}
+            checked={color === colorId}
+            name="colors"
+            className="hidden"
+            onChange={(e) => {
+              setColor(e.target.value);
+            }}
+          />
+          <label
+            htmlFor={colorId}
+            className="flex items-center cursor-pointer flex-1 gap-4"
+          >
+            <S.chip bg={bgColor} />
+            <span
+              className={`flex-1 ${
+                color === colorId ? 'font-black opacity-100' : 'opacity-30'
+              }`}
+            >
+              {name}
+            </span>
+            {color === colorId && <FaCheck />}
+          </label>
+        </div>
+      ))}
+      <Button
+        classProp="w-80 h-14 mt-3 text-lg bg-blue-600 text-white hover:bg-blue-700"
+        onClick={() => doSomething()}
+      >
+        확인
+      </Button>
     </div>
   );
 };
