6ad080619ec77763b1c401ab62bc81086e52609f
rebi13 <rebi13@naver.com>
2024-02-13 23:44:26 +0900

    style: 메모 작성 폼 스타일 추가 rebi13/MBTI-Inside#38

--- frontend/src/components/pages/Memo/Note/index.tsx
+++ frontend/src/components/pages/Memo/Note/index.tsx
@@ -6,9 +6,9 @@ const Note = () => {
   //   const { openModal } = useModalContext();
   // openModal(<MBTITypes />, null, 'MBTI 선택')
   return (
-    <div className="w-full">
-      <div className="mb-4">
-        {'ESTJ'}
+    <div className="w-full bg-black">
+      <div className="flex mb-4 items-center justify-between px-4">
+        <div className="text-4xl font-bold text-white">{'ESTJ'}</div>
         <Button onClick={() => {}}>
           <FaExchangeAlt />
         </Button>
@@ -17,24 +17,26 @@ const Note = () => {
         <input
           type="text"
           placeholder="제목"
-          className="input input-bordered w-full outline-0 mb-6"
+          className="input input-bordered w-full outline-0 mb-6 p-3 bg-inherit text-white border-solid border-white"
         />
         <textarea
-          className="textarea textarea-bordered w-full outline-0 resize-none h-5/6 p-3"
+          className="textarea textarea-bordered w-full outline-0 h-80 resize-none mb-6 p-3 text-base bg-inherit text-white border-solid border-white"
           placeholder="내용"
         />
+        <div>
+          <Button classProp={'w-full h-14 text-lg text-white mb-6 bg-inherit'}>
+            <span>배경 색상</span>
+            <div
+              className={`w-5 h-5 ml-3 ${'bg-[#FF9D42]'}`} // selected Color
+            ></div>
+          </Button>
+        </div>
+        <div>
+          <Button classProp={'w-full h-14 text-lg text-white bg-inherit'}>
+            작성 완료
+          </Button>
+        </div>
       </form>
-      <div>
-        <Button>
-          <span>배경 색상</span>
-          <div
-            className={`w-5 h-5 ml-3 ${'bg-[#FF9D42]'}`} // selected Color
-          ></div>
-        </Button>
-      </div>
-      <div>
-        <Button>작성 완료</Button>
-      </div>
     </div>
   );
 };
