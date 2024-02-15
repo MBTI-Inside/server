9b55cc491b2aa265f92dcd6fbcd9543332fc8cf1
rebi13 <rebi13@naver.com>
2024-02-15 23:58:40 +0900

    docs: memo view header 스토리북 rebi13/MBTI-Inside#44

---
+++ frontend/src/components/pages/Memo/MemoViewHeader/index.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import MemoViewHeader from '@/components/pages/Memo/MemoViewHeader';
+
+const meta = {
+  title: 'components/pages/Memo/MemoViewHeader',
+  component: MemoViewHeader,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs'],
+  argTypes: {}
+} satisfies Meta<typeof MemoViewHeader>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ShortNote: Story = {
+  args: {}
+};
