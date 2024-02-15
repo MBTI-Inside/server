af95a2bb1332f271e2fac187df1506f8da3c269f
rebi13 <rebi13@naver.com>
2024-02-15 23:38:07 +0900

    feat: memo view header rebi13/MBTI-Inside#44

---
+++ frontend/src/components/pages/Memo/MemoViewHeader/index.tsx
@@ -0,0 +1,11 @@
+const MemoViewHeader = () => {
+  return (
+    <div className="flex flex-row w-full justify-between h-12">
+      <div>{'<'}</div>
+      <div>ESTJ</div>
+      <div>hamburgur</div>
+    </div>
+  );
+};
+
+export default MemoViewHeader;
