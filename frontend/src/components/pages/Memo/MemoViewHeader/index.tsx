1ced39308e8b1424eadc4db65d0ef4234afb55f4
rebi13 <rebi13@naver.com>
2024-02-16 00:00:13 +0900

    Revert "style: memo view header 퍼블리싱 rebi13/MBTI-Inside#44"
    This reverts commit 862f064c4fec0e0a62908848d485d345a07577ea.

--- frontend/src/components/pages/Memo/MemoViewHeader/index.tsx
+++ frontend/src/components/pages/Memo/MemoViewHeader/index.tsx
@@ -1,36 +1,9 @@
-import { CiMenuKebab } from 'react-icons/ci';
-import { IoIosArrowBack } from 'react-icons/io';
-import { useNavigate } from 'react-router-dom';
-
 const MemoViewHeader = () => {
-  const nav = useNavigate();
-
   return (
-    <div className="flex flex-row w-full justify-between items-center h-12">
-      <div className="pl-4 text-xl">
-        <IoIosArrowBack onClick={() => nav(-1)} />
-      </div>
-      <div className="text-xl">ESTJ</div>
-      <div className="dropdown dropdown-end">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn m-1 bg-inherit border-none text-xl"
-        >
-          <CiMenuKebab />
-        </div>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-24"
-        >
-          <li className="items-center">
-            <a>수정</a>
-          </li>
-          <li className="items-center">
-            <a>삭제</a>
-          </li>
-        </ul>
-      </div>
+    <div className="flex flex-row w-full justify-between h-12">
+      <div>{'<'}</div>
+      <div>ESTJ</div>
+      <div>hamburgur</div>
     </div>
   );
 };
