6848e08d68ec55f8264f61562460fddfa80d685b
rebi13 <rebi13@naver.com>
2024-02-18 14:21:21 +0900

    style: 댓글 영역 스타일링 rebi13/MBTI-Inside#47

--- frontend/src/components/pages/Memo/Comment/CommentCard/index.tsx
+++ frontend/src/components/pages/Memo/Comment/CommentCard/index.tsx
@@ -1,10 +1,12 @@
 import { CiMenuKebab } from 'react-icons/ci';
+import { IoHeartOutline } from 'react-icons/io5';
 
+import Button from '@/components/common/Button';
 import * as S from '@/components/pages/Memo/Comment/CommentCard/index.styles';
 
 const CommentCard = () => {
   return (
-    <div className="flex flex-col shadow-xl">
+    <div className="flex flex-col shadow-xl bg-green-200 mb-3 rounded-xl p-2">
       <div className="flex justify-between items-center">
         <div className="flex items-center">
           <S.UserIcon />
@@ -27,11 +29,20 @@ const CommentCard = () => {
         </div>
       </div>
       <div className="flex flex-col">
-        <div>comment comment comment </div>
-        <div>2024-02-14 17:18</div>
-        <div className="flex justify-between">
-          <button>답글</button>
-          <button>공감</button>
+        <div className="mb-1">
+          comment comment commentcomment comment commentcomment comment
+          commentcomment comment commentcomment comment comment{' '}
+        </div>
+        <div className="mb-1">2024-02-14 17:18</div>
+        <div className="flex justify-between items-center">
+          <Button classProp={'h-6 rounded-none'}>답글</Button>
+          <div className="flex items-center">
+            <IoHeartOutline
+              className="cursor-pointer"
+              onClick={() => alert('좋아요 클릭 or 취소')}
+            />
+            <span>13</span>
+          </div>
         </div>
       </div>
     </div>
