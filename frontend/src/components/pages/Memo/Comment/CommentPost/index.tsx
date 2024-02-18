2fa8f910b269676a6ac01f4b96ab456d7076fb0b
rebi13 <rebi13@naver.com>
2024-02-18 14:20:09 +0900

    style: 댓글 등록 폼 영역 rebi13/MBTI-Inside#47

--- frontend/src/components/pages/Memo/Comment/CommentPost/index.tsx
+++ frontend/src/components/pages/Memo/Comment/CommentPost/index.tsx
@@ -2,9 +2,9 @@ import Button from '@/components/common/Button';
 
 const CommentPost = () => {
   return (
-    <div className="flex">
-      <textarea className="textarea textarea-bordered textarea-xs w-full max-w-xs" />
-      <Button classProp={'ml-3'}>등록</Button>
+    <div className="flex mb-3">
+      <textarea className="h-10 textarea-bordered textarea-xs w-full max-w-xs rounded-full" />
+      <Button classProp={'ml-2 h-10'}>등록</Button>
     </div>
   );
 };
