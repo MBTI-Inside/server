5cc73019595843172544271bf7beb42a9cb4f3c8
rebi13 <rebi13@naver.com>
2024-02-18 14:21:02 +0900

    style: 댓글 영역 위치 조정 rebi13/MBTI-Inside#47

--- frontend/src/components/pages/Memo/Comment/index.tsx
+++ frontend/src/components/pages/Memo/Comment/index.tsx
@@ -4,7 +4,7 @@ import CommentPost from '@/components/pages/Memo/Comment/CommentPost';
 const Comment = () => {
   return (
     <div className="px-6 w-full">
-      <div className="flex justify-between items-center mb-6">
+      <div className="flex justify-between items-center mb-4">
         <div className="text-xl">댓글</div>
         <div className="flex ">
           <div className="text-bold">등록순</div>
