8fbb913bc8b9973a971aee7d431cb030d8d2c30d
rebi13 <rebi13@naver.com>
2024-02-24 17:53:42 +0900

    style: 디자인 수정 (공통 Button 활용) rebi13/MBTI-Inside#62

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/common/Button';
 import Character from '@/components/common/Character';
 
 import Google from '@/assets/image/google.svg?react';
@@ -11,22 +12,40 @@ const Login = () => {
       <div>
         <Character bgcolor="#FF42B3" gcolor="#F9BAAC" />
         <div className="flex flex-col bg-[#FF42B3] items-center gap-2">
-          <div className="flex items-center w-5/6 py-1 justify-center gap-4 bg-[#FEE501] rounded-full shadow-lg">
+          <Button
+            classProp={
+              'flex items-center w-5/6 py-1 justify-center gap-4 bg-[#FEE501] rounded-full shadow-lg border-none'
+            }
+          >
             <Kakao />
             <span className="text-md w-32 text-center">카카오로 함께 하기</span>
-          </div>
-          <div className="flex items-center w-5/6 py-1 justify-center gap-4 bg-[#03C75A] rounded-full shadow-lg text-white">
+          </Button>
+
+          <Button
+            classProp={
+              'flex items-center w-5/6 py-1 justify-center gap-4 bg-[#03C75A] rounded-full shadow-lg border-none text-white'
+            }
+          >
             <Naver />
             <span className="text-md w-32 text-center">네이버로 함께 하기</span>
-          </div>
-          <div className="flex items-center w-5/6 py-1 justify-center gap-4 bg-[#FFFFFF] rounded-full shadow-lg">
+          </Button>
+
+          <Button
+            classProp={
+              'flex items-center w-5/6 py-1 justify-center gap-4 bg-[#FFFFFF] rounded-full shadow-lg border-none'
+            }
+          >
             <Google />
             <span className="text-md w-32 text-center">구글로 함께 하기</span>
-          </div>
-          <div className="flex items-center w-5/6 py-1 justify-center gap-4 bg-[#B2ACF9] rounded-full shadow-lg">
+          </Button>
+          <Button
+            classProp={
+              'flex items-center w-5/6 py-1 justify-center gap-4 bg-[#B2ACF9] rounded-full shadow-lg border-none'
+            }
+          >
             <Logo />
             <span className="text-md w-32 text-center">그냥 함께 하기</span>
-          </div>
+          </Button>
         </div>
       </div>
     </div>
