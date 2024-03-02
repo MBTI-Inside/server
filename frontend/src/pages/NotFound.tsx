aac6b092ce6eb5f8a5fb26ed809f174b9495ab31
rebi13 <rebi13@naver.com>
2024-03-02 23:51:37 +0900

    style: 캐릭터, 스타일 추가 rebi13/MBTI-Inside#74

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,22 @@
+import Character from '@/components/common/Character';
+
 export default function NotFound() {
   return (
-    <section className="flex flex-col items-center">
-      <h3 className="text-2xl font-bold mb-2">NotFound😅</h3>
-    </section>
+    <div className="flex flex-col my-5">
+      <Character bgcolor="#00B26E" gcolor="#F9BAAC" />
+      <h3
+        className="font-bold
+            text-6xl
+            text-center
+            pb-[60px]
+            text-[#000]
+            bg-[#00B26E]"
+      >
+        No data
+      </h3>
+    </div>
+    // <section className="flex flex-col items-center">
+    //   <h3 className="text-2xl font-bold mb-2">NotFound😅</h3>
+    // </section>
   );
 }
