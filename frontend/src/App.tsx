2f8337d91c9bb74ef409e36e434a951385922c2e
rebi13 <rebi13@naver.com>
2024-03-03 13:53:04 +0900

    style: 스타일드 컴포넌트화 rebi13/MBTI-Inside#66

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import routePaths from '@/routers';
 import { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import tw from 'tailwind-styled-components';
 
 import ModalProvider from '@/hooks/useModal';
 
@@ -10,11 +11,12 @@ import Header from '@/components/layout/Header';
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
+      <AppWrapper>
         <ModalProvider>
+          {/* TODO: fallback 상태에서 로딩 컴포넌트 퍼블리싱 */}
           <Suspense fallback={<>loading...</>}>
             <Header />
-            <div className="flex-1 overflow-y-auto">
+            <ContentWrapper>
               <Routes>
                 {routePaths.map((route) => (
                   <Route
@@ -24,14 +26,24 @@ function App() {
                   />
                 ))}
               </Routes>
-            </div>
-
+            </ContentWrapper>
             <Footer />
           </Suspense>
         </ModalProvider>
-      </div>
+      </AppWrapper>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const AppWrapper = tw.div`
+  flex 
+  flex-col 
+  min-h-screen
+`;
+
+const ContentWrapper = tw.div`
+  flex-1 
+  overflow-y-auto"
+`;
