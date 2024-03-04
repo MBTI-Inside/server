e2d5c732ec6a02535644d4fa765b1038eeb65383
rebi13 <rebi13@naver.com>
2024-03-05 01:05:03 +0900

    feat: Test Title rebi13/MBTI-Inside#71

--- frontend/src/components/pages/Test/Title/index.tsx
+++ frontend/src/components/pages/Test/Title/index.tsx
@@ -1,5 +1,14 @@
+import * as S from '@/components/pages/Test/Title/styles';
+
 const TestTitle = () => {
-  return <>TestTitle</>;
+  return (
+    <S.TestTitleContainer>
+      <S.TestNumber>01</S.TestNumber>
+      <S.TestQuestion>
+        친구와 1시간이 넘는 통화를 마친 뒤 당신의 상태는?
+      </S.TestQuestion>
+    </S.TestTitleContainer>
+  );
 };
 
 export default TestTitle;
---
+++ frontend/src/components/pages/Test/Title/styles.ts
@@ -0,0 +1,24 @@
+import tw from 'tailwind-styled-components';
+
+export const TestTitleContainer = tw.div`
+    h-1/3
+    select-none
+    relative
+    animate-testComponent
+`;
+
+export const TestNumber = tw.h2`
+    text-center
+    text-3xl
+    font-black
+    text-violet-400
+`;
+
+export const TestQuestion = tw.h3`
+    text-center 
+    w-80 
+    text-xl 
+    font-semibold 
+`;
+
+// text-slate-50
