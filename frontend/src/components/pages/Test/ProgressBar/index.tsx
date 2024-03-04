8466aba3e0ac23e6fb1410476dc95886e8f07e91
rebi13 <rebi13@naver.com>
2024-03-05 01:10:12 +0900

    feat: progress bar 기본 틀 rebi13/MBTI-Inside#71

--- frontend/src/components/pages/Test/ProgressBar/index.tsx
+++ frontend/src/components/pages/Test/ProgressBar/index.tsx
@@ -1,5 +1,10 @@
 const ProgressBar = () => {
-  return <>ProgressBar</>;
+  return (
+    <div>
+      <div>1/10</div>
+      <progress className="progress w-56" value="10" max="100"></progress>
+    </div>
+  );
 };
 
 export default ProgressBar;
