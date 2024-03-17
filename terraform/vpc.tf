58b2481d6f73c1e26512d0a7e204a68370b15c25
MCprotein <chss3339@gmail.com>
2024-03-17 14:42:27 +0900

    feat: vpc 설정

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,8 +10,6 @@ resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
 
-  availability_zone = "ap-northeast-2a"
-
   tags = {
     Name = "MBTI-VPC-public-subnet"
   }
@@ -20,6 +18,8 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.10.0/24"
+
+  availability_zone = "ap-northeast-2a"
 
   tags = {
     Name = "MBTI-VPC-private-subnet"
@@ -45,9 +45,46 @@ resource "aws_eip" "nat" {
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.nat.id
 
-  subnet_id = aws_subnet.private_subnet.id
+  subnet_id = aws_subnet.public_subnet.id
 
   tags = {
     Name = "MBTI-NATGW"
   }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "MBTI-RT-public"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association_public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "MBTI-RT-private"
+  }
+}
+
+resource "aws_route_table_association" "route_table_association_private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.natgw.id
 }
