95f23887b6652d39994b29155d8e16ffd25f5c13
MCprotein <chss3339@gmail.com>
2024-03-27 00:49:58 +0900

    fix: terraform datetime

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -18,16 +18,16 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.10.0/24"
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.10.0/24"
 
-  availability_zone = "ap-northeast-2a"
+#   availability_zone = "ap-northeast-2a"
 
-  tags = {
-    Name = "MBTI-VPC-private-subnet"
-  }
-}
+#   tags = {
+#     Name = "MBTI-VPC-private-subnet"
+#   }
+# }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
@@ -37,23 +37,23 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_eip" "nat" {
-  domain = "vpc"
+# resource "aws_eip" "nat" {
+#   domain = "vpc"
 
-  lifecycle {
-    create_before_destroy = true
-  }
-}
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
 
-resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.nat.id
+# resource "aws_nat_gateway" "natgw" {
+#   allocation_id = aws_eip.nat.id
 
-  subnet_id = aws_subnet.public_subnet.id
+#   subnet_id = aws_subnet.public_subnet.id
 
-  tags = {
-    Name = "MBTI-NATGW"
-  }
-}
+#   tags = {
+#     Name = "MBTI-NATGW"
+#   }
+# }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -73,24 +73,24 @@ resource "aws_route_table_association" "route_table_association_public" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "MBTI-RT-private"
-  }
-}
+#   tags = {
+#     Name = "MBTI-RT-private"
+#   }
+# }
 
-resource "aws_route_table_association" "route_table_association_private" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "route_table_association_private" {
+#   subnet_id      = aws_subnet.private_subnet.id
+#   route_table_id = aws_route_table.private.id
+# }
 
-resource "aws_route" "private_nat" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.natgw.id
-}
+# resource "aws_route" "private_nat" {
+#   route_table_id         = aws_route_table.private.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.natgw.id
+# }
 
 # security group
 resource "aws_security_group" "backend_server_sg" {
