a7bd3ff98eade1c7e79bdbdee547f22bfcf94a48
MCprotein <chss3339@gmail.com>
2024-04-04 00:28:34 +0900

    fix: terraform ecs task_definition은 github actions를 위해 json 파일에 작성할 예정

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -7,33 +7,29 @@ resource "aws_ecs_cluster" "this" {
   }
 }
 
-resource "aws_ecs_task_definition" "mbti_backend_server" {
+# resource "aws_ecs_service" "mbti_backend_server" {
+#   name            = "mbti_backend_server"
+#   cluster         = aws_ecs_cluster.this.id
+#   task_definition = aws_ecs_task_definition.mbti_backend_server.arn
+#   desired_count   = 1
 
-}
+#   ordered_placement_strategy {
+#     type  = "binpack"
+#     field = "cpu"
+#   }
 
-resource "aws_ecs_service" "mbti_backend_server" {
-  name            = "mbti_backend_server"
-  cluster         = aws_ecs_cluster.this.id
-  task_definition = aws_ecs_task_definition.mbti_backend_server.arn
-  desired_count   = 1
+#   ordered_placement_strategy {
+#     type  = "spread"
+#     field = "attribute:ecs.availability-zone"
+#   }
 
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
+#   force_new_deployment = true
+#   triggers = {
+#     redeployment = plantimestamp()
+#   }
 
-  ordered_placement_strategy {
-    type  = "spread"
-    field = "attribute:ecs.availability-zone"
-  }
-
-  force_new_deployment = true
-  triggers = {
-    redeployment = plantimestamp()
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [ap-northeast-2]"
-  }
-}
+#   placement_constraints {
+#     type       = "memberOf"
+#     expression = "attribute:ecs.availability-zone in [ap-northeast-2]"
+#   }
+# }
