a7fa79caa093e32a27d07bc6d1e90d3c9fb5c47b
MCprotein <chss3339@gmail.com>
2024-04-11 21:19:34 +0900

    fix: backend serverless 전환

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { winstonLogger } from './util/logger';
+import { Callback, Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@codegenie/serverless-express';
+
+let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,6 +16,17 @@ async function bootstrap() {
     credentials: true
   });
 
-  await app.listen(process.env.PORT);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
+};
