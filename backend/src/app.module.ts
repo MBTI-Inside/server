269b5cfc7dc0ee97406aa9ad53de94a98d659b20
MCprotein <chss3339@gmail.com>
2024-04-21 22:11:32 +0900

    fix: joi env validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,27 @@ import { ConfigModule } from '@nestjs/config';
 import { HealthModule } from './health/health.module';
 import { BoardModule } from './board/board.module';
 import { LoggerModule } from './logger/logger.module';
+import Joi from 'joi';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync(MongooseConfig),
-    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      cache: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        MONGODB_DATABASE: Joi.string().required(),
+        MONGODB_OPTIONS: Joi.string().required(),
+        MONGODB_USERNAME: Joi.string().required(),
+        MONGODB_PASSWORD: Joi.string().required()
+      }),
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true
+      }
+    }),
     HealthModule,
     BoardModule,
     LoggerModule
