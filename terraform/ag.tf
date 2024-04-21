5977154a98e9914c0f1501c8e42855698939cde7
MCprotein <chss3339@gmail.com>
2024-04-21 22:47:58 +0900

    feta: api gateway - lambda integration

--- terraform/ag.tf
+++ terraform/ag.tf
@@ -1,22 +1,51 @@
-# resource "aws_autoscaling_group" "ecs_asg" {
-#   vpc_zone_identifier = [aws_subnet.public_subnet.id, aws_subnet.public_subnet2.id]
-#   desired_capacity    = 2
-#   max_size            = 3
-#   min_size            = 1
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "mbti-lambda-gw"
+  protocol_type = "HTTP"
+}
 
-#   launch_template {
-#     id      = aws_launch_template.ecs_launch_template.id
-#     version = "$Latest"
-#   }
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
 
-#   timeouts {
-#     delete = "30m"
-#   }
+  name        = "mbti-labmda-stage"
+  auto_deploy = true
 
-#   tag {
-#     key                 = "AmazonECSManaged"
-#     value               = true
-#     propagate_at_launch = true
-#   }
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.lambda_cloudwatch.arn
 
-# }
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri  = aws_lambda_function.test_lambda.invoke_arn
+  integration_type = "AWS_PROXY"
+}
+
+resource "aws_apigatewayv2_route" "lambda" {
+  api_id    = aws_apigatewayv2_api.lambda.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
