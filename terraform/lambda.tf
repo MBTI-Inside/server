6bb3a770526b54e80cd71b9082c3b0e3156ae653
MCprotein <chss3339@gmail.com>
2024-05-15 18:25:14 +0900

    fix: image-tag

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "mbti_backend"
   package_type  = "Image"
   role          = aws_iam_role.lambda_role.arn
-  image_uri     = "013373444325.dkr.ecr.ap-northeast-2.amazonaws.com/cicd_repository:20240515T084015-282ca84"
+  image_uri     = "013373444325.dkr.ecr.ap-northeast-2.amazonaws.com/cicd_repository:20240515T090314-cdb9e4f"
   memory_size   = "1024"
   timeout       = "900"
 
