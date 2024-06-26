62e43428d82ab8041477b572eca272ab2ed548f2
MCprotein <chss3339@gmail.com>
2024-06-26 22:38:06 +0900

    feat: error test swagger

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -5,13 +5,20 @@ import {
   HttpStatus,
   Query
 } from '@nestjs/common';
+import { ApiPropertyOptional, ApiResponseProperty } from '@nestjs/swagger';
 
 @Controller('health')
 export class HealthController {
+  @ApiPropertyOptional({
+    name: 'http status code',
+    enum: HttpStatus,
+    enumName: 'HttpStatusCode'
+  })
+  @ApiResponseProperty({ enum: HttpStatus })
   @Get()
-  getHealth(@Query('error') errorCode?: HttpStatus) {
-    if (errorCode) {
-      throw new HttpException({ status: errorCode, error: 'error' }, errorCode);
+  getHealth(@Query('status') status?: HttpStatus) {
+    if (status) {
+      throw new HttpException({ status, error: 'error' }, status);
     }
     return { status: 'OK' };
   }
